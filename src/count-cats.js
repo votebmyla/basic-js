2370e1e4b892aa287b3f62eb3c21aa9e11366825
votebmyla <votebmyla@gmail.com>
2021-02-20 21:36:29 +0500

    feat: add catscount

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(array) {
+//  throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  let cat = "^^";
+  let count = 0;
+
+  array.forEach(function(element){
+    if(Array.isArray(element)){
+      for(let el of element){
+        if(el == cat){
+          count ++;
+        } else if(typeof element == "string" && element == cat){
+          count ++;
+        }
+      }
+    }
+  });
+  
+  return count;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(array) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  let team = '';
+  if(Array.isArray(array)){
+    for(let element of array){
+      if(typeof element == 'string'){
+        element = element.trim();
+        team = team + element[0];
+      }
+    }
+    team = team.toUpperCase().split('').sort().join('');
+  } else {
+    return false;
+  }
+
+  return team;
 };
