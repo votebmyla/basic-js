d1b14ea8f6f3da1a100f5bcfc664cfff2c778fb4
votebmyla <votebmyla@gmail.com>
2021-02-21 15:02:32 +0500

    feat: add solution what-season task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  
+  try {
+    
+  } catch (error) {
+    
+  }
+
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  
+
+  try{
+    if(date == null){
+      return 'Unable to determine the time of year!';
+    }
+
+    if(isNaN(Number(date))){
+      throw new Error(`"${date}" is not a Date() object`);
+    } else{
+
+      let month = date.getMonth();
+    
+      if(month >= 2 && month < 5){
+        return 'spring';
+      } else if (month >= 5 && month < 8){
+        return 'summer';
+      } else if (month >= 8 && month < 11){
+        return 'autumn';
+      } else if (month >= 0 && month < 2 || month == 11){
+        return 'winter';
+      }
+    }
+  }catch(err){
+    
+    throw err.name;
+  }
+
 };
