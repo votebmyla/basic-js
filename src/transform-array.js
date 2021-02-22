ada1237be7f0fe08ed766f21661b4336efad43c1
votebmyla <votebmyla@gmail.com>
2021-02-22 19:00:44 +0500

    feat: add solution transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,11 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(array) {
-  
-  try {
-    
-  } catch (error) {
-    
-  }
 
-};
+module.exports = function transform(array){
+  try{
+    if(!Array.isArray(array)){
+      throw new Error(`"${array} is not an array"`);
+    }
+    let result = array.slice(0);
+    let transformedArr = [];
+    for (let i = 0; i < result.length; i++){
+      switch(result[i]){
+        case '--discard-next':
+          delete result[i];
+          if(i != (result.length-1)){
+            delete result[i+1];  
+          }
+          break;
+        case '--discard-prev':
+          delete result[i];
+          if(i != 0){
+            result[i-1] = result[i];
+          }
+          break;
+        case '--double-next':
+         delete result[i];
+          if(i != (result.length-1)){
+            result[i] = result[i+1];
+          }
+          break;
+        case '--double-prev':
+          delete result[i];
+          if(i != 0){
+            result[i] = result[i-1];
+          }
+          break;
+      }
+    }
+    for (element of result){
+      if(element != undefined){
+        transformedArr.push(element);
+      }
+    }
+    return transformedArr;
+  } catch(err){
+    throw err.name;
+  }
+}
