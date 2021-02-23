e06bcaaab1c855ee4fcbb1f89f2428df67636ad3
votebmyla <votebmyla@gmail.com>
2021-02-23 16:39:30 +0500

    feat: add solution chainmaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(value === undefined){
+      this.chain.push(`( )~~`);
+      return this;
+    } else if(value == null){
+      this.chain.push(`( ${value} )~~`);
+      return this;
+    } else {
+      this.chain.push(`( ${value} )~~`);
+      return this;
+    }
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    try {
+      if(!Number.isInteger(position) || position > this.chain.length || position <= 0){
+        throw new Error(`"${position}" - not integer or out of range`);
+      }
+      this.chain.splice(position-1, 1);
+      return this;
+    } catch (error) {
+      this.chain.splice(0)
+      throw error.name;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.chain.join('').slice(0, -2);
+    this.chain.splice(0);
+    return result;
   }
 };
 
