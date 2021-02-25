b2ed3fe4201cc20e5593fdae89c23e4c129e1211
votebmyla <votebmyla@gmail.com>
2021-02-26 00:02:23 +0500

    feat: add solution extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  //  remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = String(str);
+  
+  let separator;
+  if(options.separator == undefined){
+    separator = options.separator;
+  }else{
+    separator = String(options.separator);
+  }
+  
+  let repeatTimes;
+  if(options.repeatTimes == undefined){
+    repeatTimes = options.repeatTimes;
+  }else{
+    repeatTimes = Number(options.repeatTimes);
+  }
+  
+  let addition; 
+  if(options.addition == undefined){
+    addition = options.addition;
+  }else{
+    addition = String(options.addition);
+  }
+    
+  let additionSeparator = String(options.additionSeparator);
+  
+  let additionRepeatTimes;
+  if(options.additionRepeatTimes == undefined){
+    additionRepeatTimes = options.additionRepeatTimes;
+  }else{
+    additionRepeatTimes = Number(options.additionRepeatTimes);
+  }
+  
+  
+  function additionHandler(addition='', additionSeparator = '|', additionRepeatTimes = 1){
+    let res = (addition + additionSeparator).repeat(additionRepeatTimes);
+    let finalResult = res.slice(0, -(additionSeparator.length));
+    return finalResult;
+  }
+  
+  function strHandler(str, separator = '+', repeatTimes = 1){
+    let res = (str+additionHandle+separator).repeat(repeatTimes);
+    let finalResult = res.slice(0, -(separator.length));
+    return finalResult;
+    
+  }
+  
+  let additionHandle = additionHandler(addition, additionSeparator, additionRepeatTimes);
+  let strHandle = strHandler(str, separator, repeatTimes);
+  
+  return strHandle;
+  
 };
   
