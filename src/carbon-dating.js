89fab9b33577ba0fc5bfcc76c55cb45348a333cd
votebmyla <votebmyla@gmail.com>
2021-02-28 21:35:17 +0500

    feat: add 100% solution carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,9 +4,12 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity){
+  if(typeof sampleActivity != 'string'){
+    return false;
+  }
   let activityNum = Number(sampleActivity);
   
-  if(activityNum < HALF_LIFE_PERIOD && activityNum > 15){
+  if(activityNum < HALF_LIFE_PERIOD && activityNum > 0){
     let age = Math.ceil(Math.log(MODERN_ACTIVITY/activityNum)/ (0.693 / HALF_LIFE_PERIOD));
     if(age<0){
       return false;
@@ -16,3 +19,4 @@ module.exports = function dateSample(sampleActivity){
     return false;
   }
 };
+
